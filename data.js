9ed5a6306b10e0db18ef97cb85505a57f338ef80
DwrUisce <bellinir@tcd.ie>
2022-11-11 11:07:59 +0000

    v1

---
+++ data.js
@@ -0,0 +1,221 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-umar-uisce-amh",
+      "name": "Umar uisce amh",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -0.04723928097379648,
+        "pitch": 0.09484865517307561,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.05426442368299966,
+          "pitch": -0.2265153975970602,
+          "rotation": 0,
+          "target": "3-iontgil-abhainn"
+        },
+        {
+          "yaw": -0.02935044416704713,
+          "pitch": 0.4404086134825125,
+          "rotation": 0,
+          "target": "1-tuirbn"
+        },
+        {
+          "yaw": 0.9232907857366683,
+          "pitch": 0.6326034070980153,
+          "rotation": 0,
+          "target": "2-seomra-rialaithe"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.43215328079588744,
+          "pitch": -0.24471497725414437,
+          "title": "<span lang=\"EN-US\" style=\"font-size:11.0pt;line-height:\n107%;font-family:&quot;Calibri&quot;,sans-serif;mso-ascii-theme-font:minor-latin;\nmso-fareast-font-family:Calibri;mso-fareast-theme-font:minor-latin;mso-hansi-theme-font:\nminor-latin;mso-bidi-font-family:&quot;Times New Roman&quot;;mso-bidi-theme-font:minor-bidi;\nmso-ansi-language:EN-US;mso-fareast-language:EN-US;mso-bidi-language:AR-SA\">Fáilte</span>",
+          "text": "<p class=\"MsoNormal\"><span lang=\"EN-US\">Fáilte go dtí turas fíorúil ar thuirbín\nuisce aisghabhála fuinnimh ag an Grúpscéim Uisce Na Staighrí Dubha. Feidhmíonn\nan córas seo mar thaispeántas ar an bhféidearthacht chun leictreachas a\naisghabháil ó phíobáin soláthair uisce atá ann cheana féin in ionad é a\nscaipeadh trí chomhla sníomhacháin. Anseo ag an Grúpscéim Uisce Na Staighrí\nDubha, asbhaintear uisce ó iontógáil abhann ag airde níos airde agus\niompraítear é trí phíblíne 2 km suas go dtí an umar uisce amh ar a bharr a\nbhfuil tú lonnaithe faoi láthair.<o:p></o:p></span></p><p class=\"MsoNormal\"><span lang=\"EN-US\"><br></span></p><p class=\"MsoNormal\"><span lang=\"EN-US\">Mar gheall ar an difríocht airde idir an\niontógáil abhann agus an umar, táirgeann uisce a thaistealaíonn de réir\ndomhantarraingthe brú barrachais a úsáidtear chun an tuirbín a thiomáint agus\ncumhacht a ghiniúint.<o:p></o:p></span></p><p class=\"MsoNormal\"><span lang=\"EN-US\">\n\n<span lang=\"EN-US\" style=\"font-size:11.0pt;line-height:107%;font-family:&quot;Calibri&quot;,sans-serif;\nmso-ascii-theme-font:minor-latin;mso-fareast-font-family:Calibri;mso-fareast-theme-font:\nminor-latin;mso-hansi-theme-font:minor-latin;mso-bidi-font-family:&quot;Times New Roman&quot;;\nmso-bidi-theme-font:minor-bidi;mso-ansi-language:EN-US;mso-fareast-language:\nEN-US;mso-bidi-language:AR-SA\">Chun dul ar aghaidh leis an turas, déan do bhealach\nchuig urlár an tuirbín, an seomra rialaithe nó an bealach isteach san abhainn\ntrí chliceáil ar na comharthaí ^.</span><br></span></p><p class=\"MsoNormal\"><span lang=\"EN-US\"><span lang=\"EN-US\" style=\"font-size:11.0pt;line-height:107%;font-family:&quot;Calibri&quot;,sans-serif;\nmso-ascii-theme-font:minor-latin;mso-fareast-font-family:Calibri;mso-fareast-theme-font:\nminor-latin;mso-hansi-theme-font:minor-latin;mso-bidi-font-family:&quot;Times New Roman&quot;;\nmso-bidi-theme-font:minor-bidi;mso-ansi-language:EN-US;mso-fareast-language:\nEN-US;mso-bidi-language:AR-SA\"><br></span></span></p><p class=\"MsoNormal\"><span lang=\"EN-US\"><span lang=\"EN-US\" style=\"font-size:11.0pt;line-height:107%;font-family:&quot;Calibri&quot;,sans-serif;\nmso-ascii-theme-font:minor-latin;mso-fareast-font-family:Calibri;mso-fareast-theme-font:\nminor-latin;mso-hansi-theme-font:minor-latin;mso-bidi-font-family:&quot;Times New Roman&quot;;\nmso-bidi-theme-font:minor-bidi;mso-ansi-language:EN-US;mso-fareast-language:\nEN-US;mso-bidi-language:AR-SA\"></span></span></p><p class=\"MsoNormal\"><span lang=\"EN-US\">Is comhoibriú é an tionscadal seo idir\nOllscoil Bangor, Coláiste na Tríonóide, Baile Átha Cliath, agus <a name=\"_Hlk118124957\">Grúpscéim Uisce Na Staighrí Dubha</a>. Bhí sé indéanta mar\ngheall ar thacaíocht ó Chlár ERDF Interreg Ireland-Wales 2014-2020, tríd an\nTionscadal Uisce Uisce.</span></p>"
+        }
+      ]
+    },
+    {
+      "id": "1-tuirbn",
+      "name": "Tuirbín",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.27968453418044703,
+        "pitch": 0.10404572589900951,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6770520983170867,
+          "pitch": -0.0425547634147172,
+          "rotation": 0,
+          "target": "2-seomra-rialaithe"
+        },
+        {
+          "yaw": -2.525038526110201,
+          "pitch": -0.5276596202879169,
+          "rotation": 0,
+          "target": "0-umar-uisce-amh"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.26330469837379233,
+          "pitch": 0.10395517558305833,
+          "title": "<span lang=\"EN-US\" style=\"font-size:11.0pt;line-height:\n107%;font-family:&quot;Calibri&quot;,sans-serif;mso-ascii-theme-font:minor-latin;\nmso-fareast-font-family:Calibri;mso-fareast-theme-font:minor-latin;mso-hansi-theme-font:\nminor-latin;mso-bidi-font-family:&quot;Times New Roman&quot;;mso-bidi-theme-font:minor-bidi;\nmso-ansi-language:EN-US;mso-fareast-language:EN-US;mso-bidi-language:AR-SA\">Tuirbín</span>",
+          "text": "<span lang=\"EN-US\" style=\"font-size:11.0pt;line-height:\n107%;font-family:&quot;Calibri&quot;,sans-serif;mso-ascii-theme-font:minor-latin;\nmso-fareast-font-family:Calibri;mso-fareast-theme-font:minor-latin;mso-hansi-theme-font:\nminor-latin;mso-bidi-font-family:&quot;Times New Roman&quot;;mso-bidi-theme-font:minor-bidi;\nmso-ansi-language:EN-US;mso-fareast-language:EN-US;mso-bidi-language:AR-SA\">Tá\nan tuirbín seo díorthaithe ó ghnáthchaidéal uisce a athraíodh chun oibriú ar\nais mar thuirbín. Is é 3.3 kW an t-aschur cumhachta uasta, agus tá an t-aonad\ncúpláilte le gineadóir ionduchtaithe 5.5 kW. Lean ar aghaidh leis an turas trí\nchliceáil ar an gcomhartha ^ ar an tseid chun dul ar aghaidh chuig an Seomra\nRialaithe, nó cliceáil ar ais ar an dara comhartha ^ chun filleadh ar an umar\nuisce amh.</span>"
+        }
+      ]
+    },
+    {
+      "id": "2-seomra-rialaithe",
+      "name": "Seomra Rialaithe",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.412009182926802,
+        "pitch": -0.0006676428149301472,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9455466150686593,
+          "pitch": -0.031115014152447884,
+          "rotation": 0,
+          "target": "0-umar-uisce-amh"
+        },
+        {
+          "yaw": -1.2001495518336451,
+          "pitch": -0.022242651683935577,
+          "rotation": 0,
+          "target": "1-tuirbn"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.02601450582973186,
+          "pitch": -0.1866608756354431,
+          "title": "<span lang=\"EN-US\" style=\"font-size:11.0pt;line-height:\n107%;font-family:&quot;Calibri&quot;,sans-serif;mso-ascii-theme-font:minor-latin;\nmso-fareast-font-family:&quot;Times New Roman&quot;;mso-fareast-theme-font:minor-fareast;\nmso-hansi-theme-font:minor-latin;mso-bidi-font-family:&quot;Times New Roman&quot;;\nmso-bidi-theme-font:minor-bidi;mso-ansi-language:EN-US;mso-fareast-language:\nEN-US;mso-bidi-language:AR-SA\">Seomra Rialaithe</span>",
+          "text": "<p class=\"MsoNormal\"><span lang=\"EN-US\">Tá an comh-aireachta rialaithe seo\nfreagrach as an chumhacht a ghineann an tuirbín a bheathú isteach sa ghléasra\ncóireála uisce, agus dá bhrí sin méid comhionann leictreachais a fhritháireamh\na allmhaireofaí ón ngreille ar shlí eile. Tabhair faoi deara le do thoil nach\ndtagraíonn ach an comh-aireachta beag seo don tuirbín, agus tá an trealamh eile\nsa seomra nasctha le codanna eile de na hoibreacha cóireála uisce. <o:p></o:p></span></p>\n\n<span lang=\"EN-US\" style=\"font-size:11.0pt;line-height:107%;font-family:&quot;Calibri&quot;,sans-serif;\nmso-ascii-theme-font:minor-latin;mso-fareast-font-family:Calibri;mso-fareast-theme-font:\nminor-latin;mso-hansi-theme-font:minor-latin;mso-bidi-font-family:&quot;Times New Roman&quot;;\nmso-bidi-theme-font:minor-bidi;mso-ansi-language:EN-US;mso-fareast-language:\nEN-US;mso-bidi-language:AR-SA\">Lean are aghaidh leis an turas trí chliceáil ar\nan gcomhartha ^ ar an doras chun filleadh ar an umar uisce amh, nó cliceáil ar\nais ar an dara comhartha ^ chun filleadh ar an tuirbín.</span>"
+        }
+      ]
+    },
+    {
+      "id": "3-iontgil-abhainn",
+      "name": "Iontógáil abhainn",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -2.933710229191643,
+        "pitch": 0.19897857804654606,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.634956654662937,
+          "pitch": -0.16583944150071517,
+          "rotation": 0,
+          "target": "0-umar-uisce-amh"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.837459123186468,
+          "pitch": 0.1668146138462845,
+          "title": "<span lang=\"EN-US\" style=\"font-size:11.0pt;line-height:\n107%;font-family:&quot;Calibri&quot;,sans-serif;mso-ascii-theme-font:minor-latin;\nmso-fareast-font-family:Calibri;mso-fareast-theme-font:minor-latin;mso-hansi-theme-font:\nminor-latin;mso-bidi-font-family:&quot;Times New Roman&quot;;mso-bidi-theme-font:minor-bidi;\nmso-ansi-language:EN-US;mso-fareast-language:EN-US;mso-bidi-language:AR-SA\">Iontógáil\nabhainn</span>",
+          "text": "<p class=\"MsoNormal\"><span lang=\"EN-US\">Déantar an t-uisce a asbhaint mar gheall ar\natreorú ar an abhainn agus seolta chuig umar socraithe (an ciúb coincréite). Ón\náit seo, imíonn an phíblíne 2 km ar fad a théann chuig an tuirbín agus ar\ndeireadh go dtí na hoibreacha cóireála amhuisce.<o:p></o:p></span></p>\n\n<span lang=\"EN-US\" style=\"font-size:11.0pt;line-height:107%;font-family:&quot;Calibri&quot;,sans-serif;\nmso-ascii-theme-font:minor-latin;mso-fareast-font-family:Calibri;mso-fareast-theme-font:\nminor-latin;mso-hansi-theme-font:minor-latin;mso-bidi-font-family:&quot;Times New Roman&quot;;\nmso-bidi-theme-font:minor-bidi;mso-ansi-language:EN-US;mso-fareast-language:\nEN-US;mso-bidi-language:AR-SA\">Lean ar aghaidh leis an turas trí chliceáil ar\nan gcomhartha ^ chun filleadh ar an umar uisce amh</span>"
+        },
+        {
+          "yaw": -2.116138120363443,
+          "pitch": 0.21114879841042367,
+          "title": "<p class=\"MsoNormal\"><span lang=\"EN-US\">Go raibh maith agat<o:p></o:p></span></p>",
+          "text": "<p class=\"MsoNormal\"><span lang=\"EN-US\">Nuair a chríochnaíonn tú ag féachaint\ntimpeall ar an ardán seo,&nbsp;<o:p></o:p></span><span style=\"font-family: Calibri, sans-serif; font-size: 11pt;\">cuireann\nsé seo deireadh lenár dturas.&nbsp;</span><span style=\"font-family: Calibri, sans-serif; font-size: 11pt;\">Go\nraibh maith agat as do spéis inár dtionscadal!</span></p>"
+        }
+      ]
+    }
+  ],
+  "name": "BlackstairsG",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
